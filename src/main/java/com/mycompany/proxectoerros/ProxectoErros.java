ebba57931555a61e019b2406b4e044e938bd91aa
a21pablorf@iessanclemente.net <>
2023-03-13 16:34:06 +0100

    Version inicial do código a depurar

---
+++ src/main/java/com/mycompany/proxectoerros/ProxectoErros.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.proxectoerros;
+
+/**
+ *
+ * @author Pablo Romero Fernandez <a21pablorf at iessanclemente.net>
+ */
+public class ProxectoErros {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int num=789;
+        int resto;
+        int numInvertido=0;
+        
+        while(num!=0){
+            resto=num/10;
+            numInvertido=numInvertido*10+resto;
+            
+        }
+        System.out.println("O numero invertido é "+numInvertido);
+    }
+}
